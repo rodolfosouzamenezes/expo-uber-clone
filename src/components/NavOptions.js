7194064c8a54c35ee98a2d310bb6f17603351f30
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-10-04 16:06:12 -0300

    feat(MapScreen): created map component

--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -3,6 +3,8 @@ import tw from 'tailwind-react-native-classnames';
 import { Icon } from '@rneui/base'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 const data = [
   {
@@ -21,6 +23,7 @@ const data = [
 
 const NavOptions = () => {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin)
 
   return (
     <FlatList
@@ -31,18 +34,19 @@ const NavOptions = () => {
         <TouchableOpacity
           onPress={() => navigation.navigate(item.screen)}
           style={tw`pr-4 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          disabled={!origin}
         >
-          <View>
+          <View style={tw`${!origin && "opacity-50"}`}>
             <Image
               style={{ width: 120, height: 120, resizeMode: 'contain' }}
               source={{ uri: item.image }}
             />
             <Text style={tw`mt-2 text-lg font-semibold text-center`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name='arrowright' color='white' type='antdesign'
+            />
           </View>
-          <Icon 
-            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-            name='arrowright' color='white' type='antdesign'
-          />
         </TouchableOpacity>
       )}
     />
