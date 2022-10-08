ee9d5cfe7d7ada9e86e24f6591a333e400704749
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-10-08 19:08:43 -0300

    feat(ride options): added uber options

--- src/components/RideOptionsCard.js
+++ src/components/RideOptionsCard.js
@@ -1,14 +1,82 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text, SafeAreaView, View, TouchableOpacity, FlatList, Image } from 'react-native'
+import React, { useState } from 'react'
+import tw from 'tailwind-react-native-classnames'
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selectedCar, setSelectedCar] = useState(null);
+
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow `}>
+      <View >
+        <TouchableOpacity
+          onPress={() => navigation.navigate('NavigateCard')}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
+          <Icon name='chevron-left' type='fontawesome' />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-4 text-xl`}>Selected a Ride</Text>
+      </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelectedCar(item)}
+            style={tw`flex-row justify-between items-center px-10 ${id === selectedCar?.id && 'bg-gray-200'}`}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: 'contain',
+              }}
+              source={{ uri: image }}
+            />
+
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel Time...</Text>
+            </View>
+
+            <Text style={tw`text-xl`}>R$ 88.90</Text>
+          </TouchableOpacity>
+        )}
+      />
+
+      <View>
+        <TouchableOpacity disabled={!selectedCar} style={tw`bg-black py-3 m-3 ${!selectedCar && 'bg-gray-300'}`}>
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selectedCar?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default RideOptionsCard
-
-const styles = StyleSheet.create({})
