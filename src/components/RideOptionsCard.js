0e512ef9b2a4689c5ec8d4ca7321265bd85e9b0d
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-10-08 22:32:52 -0300

    refactor(ride options): change for opitional atribute

--- src/components/RideOptionsCard.js
+++ src/components/RideOptionsCard.js
@@ -68,7 +68,7 @@ const RideOptionsCard = () => {
 
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>{travelTimeInformation?.duration.text}Travel Time</Text>
+              <Text>{travelTimeInformation?.duration?.text}Travel Time</Text>
             </View>
 
             <Text style={tw`text-xl`}>
